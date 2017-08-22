f4d35334fe795c3a6fd0edac114aa13172a904f8
eenblam <ben.a.elam@gmail.com>
2017-08-21 19:33:44 -0700

    Make schema.py flexible
    README.md updated to match

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # wikitor
 Basic wrapper around the MediaWiki API for scraping pages over Tor.
 
-## Installation
-
-
+## Installation and Setup
+To install the required libraries into your current environment:
 ```bash
 pip install -r requirements.txt
+```
+
+Suppose we want to stash a bunch of WikiHow articles about sewing.
+We can create a SQLite database `sewing.db` with a table called `sewing`
+using the following:
+```bash
+python schema.py sewing
 ```
--- schema.py
+++ schema.py
@@ -1,14 +1,24 @@
 import sqlite3
+import sys
 
-s = '''CREATE TABLE pages(
+try:
+    NAME = sys.argv[1]
+except IndexError:
+    print("Error: no name provided")
+    print("This script creates <name>.db with table <name>.")
+    print("Usage: python schema.py <name>")
+    sys.exit(1)
+
+s = '''CREATE TABLE {}(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     endpoint text,
     content text,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
-    );'''
+    );'''.format(NAME)
 
-conn = sqlite3.connect('pages.db')
+conn = sqlite3.connect('{}.db'.format(NAME))
 c = conn.cursor()
+c.execute('DROP TABLE IF EXISTS {}'.format(NAME))
 c.execute(s)
 conn.commit()
 c.close()
